38ebd6f263a7b3af29b80275c5c2fb4ee75ace67
foxpy <muradkarammaev@gmail.com>
2018-04-29 15:41:04 +0300

    Implement simplest bot behaviour

--- bot.py
+++ bot.py
@@ -1,2 +1,42 @@
+from telegram.ext import Updater, CommandHandler
+import logging
+
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def log(command_type, chat_id, message_text):
+    logger.info("[{}] - [{}] - Received: {}".format(command_type, chat_id,
+        message_text))
+
+
+def start(bot, update):
+    update.message.reply_text("Daily random weather for role play chats.")
+    log("START", update.effective_chat.id, update.message.text)
+
+
+
+# I don't want to override Python's built-in help object
+def help_cmd(bot, update):
+    update.message.reply_text("Just type '/weather'.")
+    log("HELP", update.effective_chat.id, update.message.text)
+
+
+def error(bot, update, error):
+    logger.warning("Update \"%s\" caused error \"%s\"", update, error)
+
+
 def run(token, admin_id):
-    print("Got token: {} and admin ID: {}.".format(token, admin_id))
+    updater = Updater(token)
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help_cmd))
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
