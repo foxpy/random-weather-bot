54f42d1693682a9a01bfee3003b1939fd9af168f
foxpy <muradkarammaev@gmail.com>
2018-04-30 08:23:38 +0300

    Add humidity

--- weathergenerator.py
+++ weathergenerator.py
@@ -10,7 +10,10 @@ class WeatherGenerator:
         temperature_day = randint(-25, 35)
         temperature_night = temperature_day - randint(3, 15)
 
+        humidity = randint(20, 100)
+
         self.current_weather = f"Temperature day: {temperature_day}{degrees}\n" + \
-            f"Temperature night: {temperature_night}{degrees}"
+            f"Temperature night: {temperature_night}{degrees}\n" + \
+            f"Humidity: {humidity}%"
 
         return self.current_weather
