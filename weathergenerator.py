9a1387dcb3771097fbd4e8ebcabb19aff11d948c
Murad <muradkarammaev@gmail.com>
2018-08-25 16:54:32 +0300

    Implement get_weather()

--- weathergenerator.py
+++ weathergenerator.py
@@ -1,3 +1,5 @@
+from random import choice, randint
+
 class WeatherType:
     def __init__(self, weathers, temperature_day_range,
             temperature_night_range, wind_range, humidity_range):
@@ -10,6 +12,20 @@ class WeatherType:
                 tuple(wind_range)
         self.__min_humidity, self.__max_humidity = \
                 tuple(humidity_range)
+
+    def get_weather(self):
+        weather = choice(self.__weathers)
+        temperature_day = randint(self.__min_temp_day, self.__max_temp_day)
+        temperature_night = randint(self.__min_temp_night,
+                self.__max_temp_night)
+        humidity = randint(self.__min_humidity, self.__max_humidity)
+        wind_speed = randint(self.__min_wind_speed, self.__max_wind_speed)
+
+        return f"Weather: {weather}\n" + \
+            f"Temperature day: {temperature_day}\n" + \
+            f"Temperature night: {temperature_night}\n" + \
+            f"Humidity: {humidity}\n" + \
+            f"Wind speed: {wind_speed}\n"
 
 weather_types = \
         [WeatherType(["sunny, cloudy", "rain"],
